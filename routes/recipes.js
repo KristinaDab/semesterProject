10fe3d434fa70ecf36844a62369148b00239eb50
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2019-11-07 12:54:42 +0100

    Update comment

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-//Initializing queries to avoid long strings in the code
+//Initializing mysql queries to avoid long strings in the code
 var query1 = 'SELECT r.title, r.recipe_id, r.cook_username, cat.category_name, r.yield, r.instructions FROM recipe AS r INNER JOIN category AS cat ON r.category_id = cat.category_id';
 
 var query2 = 'SELECT ingr.ingredient_name, ri.recipe_id, ri.quantity, u.unit_code FROM recipe AS r INNER JOIN recipe_ingredient AS ri ON r.recipe_id = ri.recipe_id INNER JOIN ingredient AS ingr ON ri.ingredient_id = ingr.ingredient_id INNER JOIN ingredient_unit AS u ON ri.unit_code = u.unit_code';
@@ -19,7 +19,6 @@ router.get('/', (req, res, next) => {
 		});
 	});
 });
-
 
 module.exports = router;
 
