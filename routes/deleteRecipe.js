9ebf85a23f128123f59ebce4713a30dfea9bc84e
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2019-11-12 19:58:22 +0100

    Remove deleteRecipe route

--- routes/deleteRecipe.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET update recipe page. */
-router.get('/', function(req, res, next) {
-	res.render('deleteRecipe', { title: 'Delete A Recipe', id: 'deleterecipe'});
-});
-
-module.exports = router;
