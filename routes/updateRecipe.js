4b57df9712f2ded600bbe6e1f31bc3e8cce55eba
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2019-11-12 19:58:51 +0100

    Remove updateRecipe route

--- routes/updateRecipe.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET update recipe page. */
-router.get('/', function(req, res, next) {
-	res.render('updateRecipe', { title: 'Update A Recipe', id: 'updaterecipe'});
-});
-
-module.exports = router;
