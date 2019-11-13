391e50819ac280858ae1556aba099acacbcb43b0
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2019-11-13 15:56:56 +0100

    Add functionality for adding and deleting ingredient fields in updateR.js (recipe update)
    In update form ingredient fields for existing recipe ingredients are being added automatically, and it's possible to delete them or add new ones based on parent and child dynamic id's

--- public/javascripts/updateR.js
+++ public/javascripts/updateR.js
@@ -7,10 +7,10 @@ var editBtn = document.getElementsByClassName("editBtn");
 // Loop through all the buttons and add event listener to each of them
 // that opens the modal with the same index
 for( let i = 0; i < editBtn.length; i ++){
-    editBtn[i].addEventListener("click", function(){
+	editBtn[i].addEventListener("click", function(){
     	// console.log(modals[i]);
-      	 modals[i].style.display = "block";
-		
+    	modals[i].style.display = "block";
+
     } );
 }
 
@@ -25,4 +25,61 @@ for(let i= 0; i <spans.length; i ++) {
 	});
 }
 
+// Add counter for newly added ingredients. The reason is such a 
+//high number is to avoid the same id's between other recipes ingredients
+//in the same page. 
 
+let counter = 200;
+
+// Add new ingredient fields
+
+function addIngrFields(element){
+
+	counter++;
+
+	let newdiv = document.createElement('div');
+
+	newdiv.id = counter;
+
+	let deleteBtn = '<a style="text-align:right; margin-right:50px; background-color: #ffb84f; color: white; text-decoration: none; padding: 8px 10px; display: inline-block;" href="javascript:deleteExistingFields('+ counter +')">Delete</a>';
+
+	newdiv.innerHTML = document.getElementById('addIngrTemplate').innerHTML + deleteBtn;
+
+
+	//give new names to new ingredient fields 
+
+	divChildren = newdiv.childNodes;
+
+	for (var i=0; i < divChildren.length; i++) {
+
+		divChildren[i].name = divChildren[i].name + counter; 
+	}
+
+
+	//add new ingredient fields (newdiv) to the 'addNewIngredients' div 
+	//that has a unique id depending on the recipe
+
+	let parent = document.getElementsByClassName('addNewIngredients')
+
+	for(var i=0; i<parent.length; ++i) {
+
+		if(parent[i].id === element.id){
+			parent[i].appendChild(newdiv)
+		}
+	}
+}
+
+// Remove ingredient fields
+
+function deleteExistingFields(eleId) {
+
+	d = document;
+	let ele = d.getElementById(eleId);
+	let parentEle = d.getElementsByClassName('addNewIngredients')
+	console.log(ele);
+	for(var i=0; i<parentEle.length; ++i){
+		if(parentEle[i].contains(ele)){
+			parentEle[i].removeChild(ele);
+		}
+	}
+}
