19a756ed96445e7f9badadd6510380564db1fb62
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2019-11-13 15:58:44 +0100

    Add more queries to myrecipes.js
    In order to get more information from the database for "update recipe" functionality

--- routes/myrecipes.js
+++ routes/myrecipes.js
@@ -3,24 +3,30 @@ var router = express.Router();
 var db = require('../db');
 
 
+var query = 'SELECT * FROM category ORDER BY category_name';
 var query1 = 'SELECT r.title, r.recipe_id, r.cook_username, cat.category_name, r.yield, r.instructions FROM recipe AS r INNER JOIN category AS cat ON r.category_id = cat.category_id';
-var query2 = 'SELECT ingr.ingredient_name, ri.recipe_id, ri.quantity, u.unit_code FROM recipe AS r INNER JOIN recipe_ingredient AS ri ON r.recipe_id = ri.recipe_id INNER JOIN ingredient AS ingr ON ri.ingredient_id = ingr.ingredient_id INNER JOIN ingredient_unit AS u ON ri.unit_code = u.unit_code';
-
+var query2 = 'SELECT ingr.ingredient_name, ingr.ingredient_id, ri.recipe_id, ri.quantity, u.unit_code FROM recipe AS r INNER JOIN recipe_ingredient AS ri ON r.recipe_id = ri.recipe_id INNER JOIN ingredient AS ingr ON ri.ingredient_id = ingr.ingredient_id INNER JOIN ingredient_unit AS u ON ri.unit_code = u.unit_code';
+var query3 = 'SELECT * FROM ingredient_unit ORDER BY unit_code';
 
 // Get the recipes from the database
 
 router.get('/', (req, res, next) => {
 
-	db.get().query(query1, (error, allrecipes, fields) => {
+	db.get().query(query, (error, categories, fields) => {
 
-		db.get().query(query2, (error, ingredients, fields) => {
-			
-			res.render('myrecipes' , { title: 'My Recipes', user: req.session.username, id: 'myrecipes', ingredients: ingredients, allrecipes : allrecipes});
+		db.get().query(query1, (error, allrecipes, fields) => {
 
+			db.get().query(query2, (error, ingredients, fields) => {
+
+				db.get().query(query3, (error, units, fields) => {
+
+					res.render('myrecipes' , { title: 'My Recipes', user: req.session.username, id: 'myrecipes', categories: categories, units: units, ingredients: ingredients, allrecipes : allrecipes});
+
+				});
+			});
 		});
 	});
 });
-
 
 module.exports = router;
 
