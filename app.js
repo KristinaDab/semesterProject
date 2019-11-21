843923b720b01e9520fc3583690de4c2f70e112f
Kristina Dabasinskaite <kristina.dabasinskaite@gmail.com>
2019-11-21 23:19:49 +0100

    Add async to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var async = require('async');
 
 var bodyParser = require('body-parser');
 var session = require('express-session');
